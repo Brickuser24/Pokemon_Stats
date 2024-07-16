49557187f62f1cc9a08d1c63471e892f3745eb52
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-16 21:42:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ coverage_options = {
 }
 
 def write(text):
-    st.markdown(f'<span style="color:white">{text}</span>', unsafe_allow_html=True)
+    st.markdown(f':white[{text}]')
 
-pokemon=st.text_input(":red[Pokemon Name]",placeholder="Enter a Pokemon's name")
+pokemon=st.text_input(":white[Pokemon Name]",placeholder="Enter a Pokemon's name")
 try:
     url = "https://pokeapi.co/api/v2/pokemon/" + pokemon.lower().rstrip().lstrip()
     data = requests.get(url).json()
