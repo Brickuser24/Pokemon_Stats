e568b1af7f3eecdb065353479a4d3c82404e7835
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-16 22:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ coverage_options = {
 }
 
 def write(text):
-    st.markdown(f':blue[{text}]')
+    st.write(f':blue[{text}]')
 
 pokemon=st.text_input("Pokemon Name", placeholder="Enter a Pokemon's name")
 try:
