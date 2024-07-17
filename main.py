8265b7aae4229f909ae6dade1035e55e4d5c72c0
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-17 09:34:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ coverage_options = {
     "Fire": ["Dragon", "Electric", "Fighting"]
 }
 
-type_colors={"Dragon":"Navy","Ghost":"DarkViolet"}
+type_colors={"Dragon":"Navy","Ghost":"Indigo"}
 
 def write(text,color="gray"):
     st.write(f':{color}[{text}]')
