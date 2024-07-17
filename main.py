5ae19cf2957f55735a52a2964a7d9a4107e22674
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-17 12:22:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ coverage_options = {
     "Poison": (["Bug", "Grass", "Electric"],"MediumOrchid"),
     "Psychic": (["Fairy", "Ghost", "Water"],"HotPink"),
     "Fighting": (["Electric", "Ice", "Fire"],"Brown"),
-    "Flying": (["Steel", "Dragon", "Fighting"],"LightSteelBlue"),
+    "Flying": (["Steel", "Dragon", "Fighting"],"PowderBlue"),
     "Grass": ["Ground", "Poison", "Rock"],
     "Ground": ["Rock", "Grass", "Dark"],
     "Bug": ["Dark", "Poison", "Ground"],
