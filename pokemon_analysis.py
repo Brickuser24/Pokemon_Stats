3b61e155b398d66361cc5bec9711335b5ce60d5a
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-21 18:54:42 +0530

    Update pokemon_analysis.py

--- pokemon_analysis.py
+++ pokemon_analysis.py
@@ -14,11 +14,12 @@ def pokemon_analysis(pokemon):
     bst=0
     for stat in data["stats"]:
         base_stats.append([stat["base_stat"],stat["stat"]["name"]])
-        bst+=stat["base_stat"]
+        if stat["stat"]["name"]!="hp"
+            bst+=stat["base_stat"]
     st.write(f":red[{base_stats}]")
     major_offense=max([base_stats[1],base_stats[3]])
     minor_offense=min([base_stats[1],base_stats[3]])
-    if major_offense[0]>=0.15*bst:
+    if major_offense[0]>=0.3*bst:
         if major_offense[0]*0.9<=minor_offense[0]:
             st.write(":red[Mixed Attacker]")
         elif major_offense[1]=="attack":
