54ebf145822fe86b4406dbd24b0fe497b83f5c5d
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-07-21 18:39:55 +0530

    Create pokemon_analysis.py

---
+++ pokemon_analysis.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+
+with open("Pokedex.txt", "r") as f:
+    read=f.read()
+    options=read.split("\n")
+    f.close()
+
+def pokemon_analysis(pokemon):
+    print(pokemon.capitalize())
+    url = "https://pokeapi.co/api/v2/pokemon/" + pokemon.lower()
+    data = requests.get(url).json()
+    base_stats = []
+    bst=0
+    for stat in data["stats"]:
+        base_stats.append([stat["base_stat"],stat["stat"]["name"]])
+        bst+=stat["base_stat"]
+    major_offense=max([base_stats[1],base_stats[3]])
+    minor_offense=min([base_stats[1],base_stats[3]])
+    if major_offense[0]>=0.15*bst:
+        if major_offense[0]-minor_offense[0]<=20:
+            st.write("Mixed Attacker")
+        elif major_offense[1]=="attack":
+            st.write("Physical Attacker")
+        else:
+            st.write("Special Attacker")
+    else:
+        st.write("Defensive")
+
+pokemon=st.selectbox("Pokemon Name", placeholder="Select a Pokemon", options=options)
+pokemon_analysis(pokemon)
